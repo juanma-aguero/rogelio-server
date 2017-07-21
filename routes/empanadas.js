b9ddccdddbdedf702197c355992873a41b598106
Juan Manuel Aguero <juanma.aguero@gmail>
2017-07-21 15:13:13 -0300

    Fix empanada

--- routes/empanadas.js
+++ routes/empanadas.js
@@ -7,6 +7,14 @@ router.get('/status', function (req, res, next) {
     res.json(empanadas.getStatus());
 });
 
+router.post('/a', function (req, res, next) {
+    res.json(empanadas.agregarPedido(req.body.tipo));
+});
+
+router.post('/c', function (req, res, next) {
+    res.json(empanadas.cerrarPedido());
+});
+
 router.post('/', function (req, res, next) {
     res.json(empanadas.abrirPedido());
 });
--- tools/empanadas.js
+++ tools/empanadas.js
@@ -8,29 +8,56 @@ var description = {
 const STATUS_IN_PROGRESS = 'in_progress';
 const STATUS_CLOSED = 'closed';
 
+var tiposDeEmpanadas = [
+    'Jamon y Queso',
+    'Verdura',
+    'Pollo',
+    'Carne'
+];
+
 var pedido = {
     status: STATUS_CLOSED,
-    items: [
-        {
-            name: 'jyq',
-            quantity: 2
-        },
-        {
-            name: 'carne',
-            quantity: 4
-        }
-    ]
+    available_types: tiposDeEmpanadas,
+    items: []
 };
+
+var addToPedido = function (variedad) {
+    var found = false;
+    for (var i = 0; i < pedido.items.length; i++) {
+        var item = pedido.items[i];
+        if (item.name == variedad) {
+            item.quantity++;
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        pedido.items.push({
+            name: variedad,
+            quantity: 1
+        });
+    }
+}
 
 module.exports = {
 
     getDescription: function () {
-        return pedido;
+        return description;
     },
 
     abrirPedido: function () {
         pedido.status = STATUS_IN_PROGRESS;
         pedido.items = [];
+        return pedido;
+    },
+
+    cerrarPedido: function () {
+        pedido.status = STATUS_CLOSED;
+        return pedido;
+    },
+
+    agregarPedido: function (variedad) {
+        addToPedido(variedad);
         return pedido;
     },
 
